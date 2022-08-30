121161c9519029952c65e0068c980012e8a5d9bc
Kingsley Solomon <kinxly@gmail.com>
2022-08-30 15:51:07 +0100

    [font choice]

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,14 @@ import '../styles/globals.scss'
 import '../axios/global.js'
 import { ChakraProvider } from '@chakra-ui/react'
 import AppContextProvider from '../context/AppContext';
-import { useEffect } from 'react';
+
 
 export default function MyApp({ Component, pageProps }) {
-  
+
   return (
     <ChakraProvider >
       <Head>
         <title>Kingsley Solomon</title>
-        <meta name="description" content="Portfolio" />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <AppContextProvider>
         <Component {...pageProps} />
---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta name="description" content="Portfolio" />
+                    <meta name="viewport" content="initial-scale=1, width=device-width" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="mobile-web-app-capable" content="yes" />
+                    < link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500&family=Inconsolata:wght@400;500&family=Inter:wght@400;500&family=JetBrains+Mono:wght@400;500&family=Nunito+Sans:ital@0;1&family=Raleway:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
+
