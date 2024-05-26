ad296847c3814c7765efd209854afcb2b04805f0
Kingsley Solomon <kinxly@gmail.com>
2024-05-26 12:16:40 +0100

    fix: project review

--- components/startMenu/ProjectDesc.jsx
+++ components/startMenu/ProjectDesc.jsx
@@ -2,73 +2,101 @@ import { Avatar, AvatarGroup, Box, Flex, Text } from "@chakra-ui/react";
 import React, { useContext } from "react";
 
 const ProjectDesc = ({ contributors, about, status, date, inModal }) => {
-  return (
-    <Flex flexDir={`column`} gap={5} mt={5}>
-      <Flex flexDir={{ base: `column`, lg: `row` }} gap={10}>
-        {/* about */}
-        <Box flex={`1`}>
-          <Text mb={1} fontWeight={`bold`} className={`font-sans`}>
-            About
-          </Text>
-          <Text fontSize={{ base: `xs`, lg: `sm` }}>{about}</Text>
-        </Box>
+    // Utility function to calculate the difference in months and years
+    const calculateTimeAgo = (dateString) => {
+        const now = new Date();
+        const givenDate = new Date(dateString);
 
-        {/* release */}
-        <Box flex={`1`}>
-          <Text mb={1} fontWeight={`bold`} className={`font-sans`}>
-            Inception
-          </Text>
-          <>
-            <Text fontSize={{ base: `xs`, lg: `sm` }}>{date}</Text>
-          </>
-        </Box>
-      </Flex>
-      <Flex flexDir={{ base: `column`, lg: `row` }} gap={10}>
-        {/* package */}
-        <Box flex={`1`}>
-          <Text mb={1} fontWeight={`bold`} className={`font-sans`}>
-            Status
-          </Text>
-          <Flex gap={3}>
-            {status ? (
-              <Text fontSize={`sm`} fontWeight={`bold`} color={`green`}>
-                {status}
-              </Text>
-            ) : (
-              <Text>N/A</Text>
-            )}
-          </Flex>
-        </Box>
-        {/* contributors */}
-        <Box flex={`1`}>
-          <Text
-            mb={1}
-            fontWeight={`bold`}
-            className={`font-sans`}
-            // color={`blue`}
-          >
-            Contributors
-          </Text>
-          <AvatarGroup size="md" max={3}>
-            {contributors ? (
-              contributors.map((dev) => {
-                return (
-                  <Avatar
-                    color={`black`}
-                    key={dev.name}
-                    name={dev.name}
-                    src={dev.img}
-                  />
-                );
-              })
-            ) : (
-              <Text>...</Text>
-            )}
-          </AvatarGroup>
-        </Box>
-      </Flex>
-    </Flex>
-  );
+        // Calculate the number of years difference
+        let yearsDifference = now.getFullYear() - givenDate.getFullYear();
+        let monthsDifference = now.getMonth() - givenDate.getMonth();
+        if (monthsDifference < 0) {
+            yearsDifference--;
+            monthsDifference += 12;
+        }
+
+        // Adjust for days within the month
+        if (now.getDate() < givenDate.getDate()) {
+            monthsDifference--;
+            if (monthsDifference < 0) {
+                yearsDifference--;
+                monthsDifference += 12;
+            }
+        }
+
+        return { yearsDifference, monthsDifference };
+    };
+
+    const { yearsDifference, monthsDifference } = calculateTimeAgo(date);
+
+    return (
+        <Flex flexDir={`column`} gap={5} mt={5}>
+            <Flex flexDir={{ base: `column`, lg: `row` }} gap={10}>
+                {/* about */}
+                <Box flex={`1`}>
+                    <Text mb={1} fontWeight={`bold`} className={`font-sans`}>
+                        About
+                    </Text>
+                    <Text fontSize={{ base: `xs`, lg: `sm` }}>{about}</Text>
+                </Box>
+
+                {/* release */}
+                <Box flex={`1`}>
+                    <Text mb={1} fontWeight={`bold`} className={`font-sans`}>
+                        Inception
+                    </Text>
+                    <>
+                        {yearsDifference > 0 ? (
+                            <span>
+                                {yearsDifference} {yearsDifference === 1 ? "year" : "years"} ago
+                            </span>
+                        ) : (
+                            <span>
+                                {monthsDifference} {monthsDifference === 1 ? "month" : "months"} ago
+                            </span>
+                        )}
+                    </>
+                </Box>
+            </Flex>
+            <Flex flexDir={{ base: `column`, lg: `row` }} gap={10}>
+                {/* package */}
+                <Box flex={`1`}>
+                    <Text mb={1} fontWeight={`bold`} className={`font-sans`}>
+                        Status
+                    </Text>
+                    <Flex gap={3}>
+                        {status ? (
+                            <Text fontSize={`sm`} fontWeight={`bold`} color={`green`}>
+                                {status}
+                            </Text>
+                        ) : (
+                            <Text>N/A</Text>
+                        )}
+                    </Flex>
+                </Box>
+                {/* contributors */}
+                <Box flex={`1`}>
+                    <Text
+                        mb={1}
+                        fontWeight={`bold`}
+                        className={`font-sans`}
+                        // color={`blue`}
+                    >
+                        Contributors
+                    </Text>
+                    <AvatarGroup size='md' max={3}>
+                        {contributors ? (
+                            contributors.map((dev) => {
+                                return <Avatar color={`black`} key={dev.name} name={dev.name} src={dev.img} />;
+                            })
+                        ) : (
+                            <Text>...</Text>
+                        )}
+                    </AvatarGroup>
+                </Box>
+            </Flex>
+        </Flex>
+    );
 };
 
 export default ProjectDesc;
--- components/startMenu/ProjectScreen.jsx
+++ components/startMenu/ProjectScreen.jsx
@@ -1,173 +1,143 @@
-import { Box, Link, Image, Flex, Text, useColorMode } from "@chakra-ui/react";
+import { Box, Link, Image, Flex, Text, useColorMode, Button } from "@chakra-ui/react";
 import React, { useContext, useRef } from "react";
 import { AppContext } from "../../context/AppContext";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 import { Bar } from "react-chartjs-2";
 // import Image from 'next/image';
 import ProjectDesc from "./ProjectDesc";
 import Modal from "./ModalDesc";
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const ProjectScreen = () => {
-  const { projectInfo } = useContext(AppContext);
-  const url = useRef();
-  const gitHub = useRef();
-  const { colorMode } = useColorMode();
-  const active = colorMode == `light` ? `darkBg` : `lightBg`;
+    const { projectInfo } = useContext(AppContext);
+    const url = useRef();
+    const gitHub = useRef();
+    const { colorMode } = useColorMode();
+    const active = colorMode == `light` ? `darkBg` : `lightBg`;
 
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: true,
-        text: `${projectInfo.name} Chart Stats.`,
-      },
-    },
-  };
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top",
+            },
+            title: {
+                display: true,
+                text: `${projectInfo.name} Chart Stats.`,
+            },
+        },
+    };
 
-  const labels = projectInfo.language;
+    const labels = projectInfo.language;
 
-  const data = {
-    labels: labels,
-    datasets: [
-      {
-        label: "Languages",
-        data: projectInfo.values,
-        backgroundColor: projectInfo.colorCode,
-      },
-    ],
-  };
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                label: "Languages",
+                data: projectInfo.values,
+                backgroundColor: projectInfo.colorCode,
+            },
+        ],
+    };
 
-  const spinUrl = () => {
-    url.current.classList.add(`spin`);
-    url.current.addEventListener(`animationend`, () => {
-      url.current.classList.remove(`spin`);
-    });
-  };
-  const spinGithub = () => {
-    gitHub.current.classList.add(`spin`);
-    gitHub.current.addEventListener(`animationend`, () => {
-      gitHub.current.classList.remove(`spin`);
-    });
-  };
+    const spinUrl = () => {
+        url.current.classList.add(`spin`);
+        url.current.addEventListener(`animationend`, () => {
+            url.current.classList.remove(`spin`);
+        });
+    };
+    const spinGithub = () => {
+        gitHub.current.classList.add(`spin`);
+        gitHub.current.addEventListener(`animationend`, () => {
+            gitHub.current.classList.remove(`spin`);
+        });
+    };
 
-  return (
-    <Box
-      pl={{ lg: 5, xl: 0 }}
-      display={`flex`}
-      flexDir={`column`}
-      color={`lemon`}
-      width={{
-        base: `initial`,
-        xl: `80%`,
-      }}
-      m={{
-        base: `initial`,
-        md: `1.5rem 0 0 auto`,
-        lg: `1.5rem 1.5rem 0 auto`,
-      }}
-    >
-      <Box className="slide-in" display={{ base: `none`, lg: `block` }}>
-        <Text
-          color={`accent`}
-          className="font-mono"
-          textTransform={`capitalize`}
-          fontWeight={`bold`}
-          fontSize={{ base: `md`, md: `2xl` }}
-        >
-          {projectInfo.name}
-        </Text>
-        <Text
-          fontWeight={`bold`}
-          fontSize={`xs`}
-          color={`accent`}
-          textAlign="left"
-        >
-          {projectInfo.category}
-        </Text>
-      </Box>
-      <Box display={{ base: `block`, lg: `none` }} textAlign={`right`}>
-        <Modal
-          title={projectInfo.name}
-          contributors={projectInfo.contributors}
-          status={projectInfo.status}
-          about={projectInfo.desc}
-          date={projectInfo.date}
-        />
-      </Box>
-      {/* Chart */}
-      <Bar options={options} data={data} />
-      <Text color={`accent`} mt="2" fontStyle={`italic`} fontSize={`xs`}>
-        <Text mr={2} fontWeight={`bolder`} as="span">
-          N.B:
-        </Text>{" "}
-        The approximate figure above shows how much of the language or framework
-        was utilized throughout the project&apos;s development. It in no way
-        reflects the language&apos;s proficiency.
-      </Text>
-      {/*  */}
-      <Flex width={`100%`} justifyContent={`space-between`} mt={5}>
-        <Flex
-          justifyContent={`start`}
-          gap={3}
-          alignItems={`center`}
-          flexDir={"row-reverse"}
-        >
-          <Link
-            target="_blank"
-            onMouseEnter={spinUrl}
-            color={colorMode == `light` ? `#339adb` : `lightBg`}
-            className="font-sans"
-            fontSize={{ base: `xs`, sm: `sm` }}
-            fontWeight={`medium`}
-            fontStyle={`italic`}
-            href={projectInfo.url}
-          >
-            Visit this site.
-          </Link>
-          <Box
-            ref={url}
-            // filter={`drop-shadow(1px 1px 1px #00000030)`}
+    return (
+        <Box
+            pl={{ lg: 5, xl: 0 }}
+            display={`flex`}
+            flexDir={`column`}
+            color={`lemon`}
             width={{
-              base: `1.6em`,
-              sm: `1.9em`,
-              md: `2em`,
+                base: `initial`,
+                xl: `80%`,
             }}
-            height={{
-              base: `1.6em`,
-              sm: `1.9em`,
+            m={{
+                base: `initial`,
+                md: `1.5rem 0 0 auto`,
+                lg: `1.5rem 1.5rem 0 auto`,
             }}
-            p={`3px`}
-          >
-            <Image
-              width={`100%`}
-              height={`100%`}
-              alt="img"
-              src="https://img.icons8.com/officel/30/000000/cloud-link--v1.png"
-            />
-          </Box>
-        </Flex>
-        {/*  */}
-        <Flex
+        >
+            <Box className='slide-in' display={{ base: `none`, lg: `block` }}>
+                <Text color={`accent`} className='font-mono' textTransform={`capitalize`} fontWeight={`bold`} fontSize={{ base: `md`, md: `2xl` }}>
+                    {projectInfo.name}
+                </Text>
+                <Text fontWeight={`bold`} fontSize={`xs`} color={`accent`} textAlign='left'>
+                    {projectInfo.category}
+                </Text>
+            </Box>
+            <Box display={{ base: `block`, lg: `none` }} textAlign={`right`}>
+                <Modal
+                    title={projectInfo.name}
+                    contributors={projectInfo.contributors}
+                    status={projectInfo.status}
+                    about={projectInfo.desc}
+                    date={projectInfo.date}
+                />
+            </Box>
+            {/* Chart */}
+            <Bar options={options} data={data} />
+            <Text color={`accent`} mt='2' fontStyle={`italic`} fontSize={`xs`}>
+                <Text mr={2} fontWeight={`bolder`} as='span'>
+                    N.B:
+                </Text>{" "}
+                The approximate figure above shows how much of the language or framework was utilized throughout the project&apos;s development. It in no way
+                reflects the language&apos;s proficiency.
+            </Text>
+            {/*  */}
+            <Flex width={`100%`} justifyContent={`space-between`} mt={5}>
+                <Button
+                    as={Link}
+                    onMouseEnter={spinUrl}
+                    justifyContent={`start`}
+                    gap={3}
+                    alignItems={`center`}
+                    flexDir={"row-reverse"}
+                    // color={colorMode == `light` ? projectInfo.status.includes(`Development`) ? `grey` :`#339adb` : `lightBg`}
+                    colorScheme={projectInfo.status?.includes(`Development`) ? `yellow` : `green`}
+                    className='font-sans'
+                    size={`sm`}
+                    fontSize={`xs`}
+                    fontWeight={`medium`}
+                    fontStyle={`italic`}
+                    target='_blank'
+                    href={projectInfo.url}
+                >
+                    <Text fontSize={`xs`}>
+                        {projectInfo.status?.includes(`Development`) ? `Interact with this app in development mode` : `Interact with this app`}{" "}
+                    </Text>
+                    <Box
+                        ref={url}
+                        // filter={`drop-shadow(1px 1px 1px #00000030)`}
+                        width={{
+                            base: `1.6em`,
+                            sm: `1.9em`,
+                            md: `2em`,
+                        }}
+                        height={{
+                            base: `1.6em`,
+                            sm: `1.9em`,
+                        }}
+                        p={`3px`}
+                    >
+                        <Image width={`100%`} height={`100%`} alt='img' src='https://img.icons8.com/officel/30/000000/cloud-link--v1.png' />
+                    </Box>
+                </Button>
+                {/*  */}
+                {/* <Flex
           gap={3}
           justifyContent={`end`}
           alignItems={`center`}
@@ -203,19 +173,19 @@ const ProjectScreen = () => {
           >
             View this project on Github.
           </Link>
-        </Flex>
-      </Flex>
-      <Box display={{ base: `none`, lg: `block` }} mt={10}>
-        <ProjectDesc
-          inModal={false}
-          contributors={projectInfo.contributors}
-          status={projectInfo.status}
-          about={projectInfo.desc}
-          date={projectInfo.date}
-        />
-      </Box>
-    </Box>
-  );
+        </Flex> */}
+            </Flex>
+            <Box display={{ base: `none`, lg: `block` }} mt={10}>
+                <ProjectDesc
+                    inModal={false}
+                    contributors={projectInfo.contributors}
+                    status={projectInfo.status}
+                    about={projectInfo.desc}
+                    date={projectInfo.date}
+                />
+            </Box>
+        </Box>
+    );
 };
 
 export default ProjectScreen;
